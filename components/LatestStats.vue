a7f7cb7b547da7c07257ef34a58d2fe3bbd47e38
Thomas Neis <76168335+tjeneis@users.noreply.github.com>
2025-06-14 18:01:57 +0200

    Refactor LatestStats.vue to improve layout by adjusting column sizes for better responsiveness, ensuring consistent display of statistics.

--- components/LatestStats.vue
+++ components/LatestStats.vue
@@ -3,11 +3,11 @@
     <VCardTitle>Latest Stats</VCardTitle>
     <VCardText>
       <VRow v-if="entry">
-        <VCol cols="12" sm="6">
+        <VCol cols="6">
           <div class="text-subtitle-2">Weight</div>
           <div class="text-body-1 text-medium-emphasis">{{ entry.weight.toFixed(1) }} kg</div>
         </VCol>
-        <VCol cols="12" sm="6">
+        <VCol cols="6">
           <div class="text-subtitle-2">BMI</div>
           <div class="text-body-1 text-medium-emphasis">
             {{ entry.bmi.toFixed(1) }}
@@ -21,7 +21,7 @@
             </VChip>
           </div>
         </VCol>
-        <VCol cols="12" sm="6">
+        <VCol cols="6">
           <div class="text-subtitle-2">Body Fat</div>
           <div class="text-body-1 text-medium-emphasis">
             {{ entry.fat_percentage.toFixed(1) }}%
@@ -35,7 +35,7 @@
             </VChip>
           </div>
         </VCol>
-        <VCol cols="12" sm="6">
+        <VCol cols="6">
           <div class="text-subtitle-2">Muscle Mass</div>
           <div class="text-body-1 text-medium-emphasis">
             {{ entry.muscle_mass.toFixed(1) }} kg
@@ -49,7 +49,7 @@
             </VChip>
           </div>
         </VCol>
-        <VCol cols="12" sm="6">
+        <VCol cols="6">
           <div class="text-subtitle-2">Body Water</div>
           <div class="text-body-1 text-medium-emphasis">
             {{ entry.body_water_percentage.toFixed(1) }}%
@@ -63,7 +63,7 @@
             </VChip>
           </div>
         </VCol>
-        <VCol cols="12" sm="6">
+        <VCol cols="6">
           <div class="text-subtitle-2">Metabolic Age</div>
           <div class="text-body-1 text-medium-emphasis">{{ entry.metabolic_age }} years</div>
         </VCol>
